efe462d44919db02b41976ba3cae173142e23e1d
Mwattanachant <s6330613013@phuket.psu.ac.th>
2021-08-17 17:44:08 +0700

    Test scanner input

--- scr/HelloJava.java
+++ scr/HelloJava.java
@@ -1,5 +1,20 @@
-public class HelloJava{
+import java.util.Scanner;
+
+public class HelloJava {
     public static void main(String[] args) {
         System.out.println("HelloWorld Yub!");
+        Scanner Obi= new Scanner(System.in);
+        int x;
+        System.out.print("Enter x:");
+        x = Obi.nextInt();
+        for(int n=1;n<=x;n++){
+            for(int i=1;i+n<=x;i++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
